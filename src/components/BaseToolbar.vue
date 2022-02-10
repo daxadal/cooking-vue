8c75a4b23a1054da9f53487461017df2e25cef4f
eric <eric@hamelyn.com>
2022-02-10 11:14:59 +0100

    BaseToolbar

---
+++ src/components/BaseToolbar.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BaseToolbar",
+});
+</script>
+
+<template>
+  <header class="toolbar">
+    <nav class="toolbar__nav">
+      <slot />
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@mixin display($vertical-align: center) {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: $vertical-align;
+  justify-content: space-between;
+}
+
+.toolbar {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  overflow: hidden;
+  background-color: var(--background-color);
+  box-shadow: 0 6px 24px -12px black;
+
+  &__nav {
+    @include display;
+
+    padding: 1rem;
+  }
+}
+</style>
