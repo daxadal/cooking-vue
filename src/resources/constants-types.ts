fd169dc271dbc5667719e3aaab43dc341e963c87
eric <eric@hamelyn.com>
2022-02-11 16:59:33 +0100

    Add ingredient and utensil zod schemas

--- src/resources/constants-types.ts
+++ src/resources/constants-types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export enum Colors {
   NEUTRAL = "neutral",
   CYAN = "cyan",
@@ -12,14 +14,22 @@ export enum IngredientType {
   END = "end",
 }
 
-export interface Ingredient {
-  id: number;
-  name: string;
-  type: IngredientType;
-}
+export const Ingredient = z.object({
+  id: z.number(),
+  name: z.string(),
+  type: z.nativeEnum(IngredientType),
+});
 
-export interface Utensil {
-  id: number;
-  name: string;
-  waitTimeInMillis: number;
-}
+export type Ingredient = z.infer<typeof Ingredient>;
+
+export const IngredientArray = z.array(Ingredient);
+
+export const Utensil = z.object({
+  id: z.number(),
+  name: z.string(),
+  waitTimeInMillis: z.number(),
+});
+
+export type Utensil = z.infer<typeof Utensil>;
+
+export const UtensilArray = z.array(Utensil);
