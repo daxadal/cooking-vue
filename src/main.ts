f0e0831e5b9af9cc6aa421c01af4e2a26bcb7015
eric <eric@hamelyn.com>
2022-02-11 10:15:10 +0100

    lint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-import "./styles/index.scss"
+import "./styles/index.scss";
 
-import App from '@/App.vue'
-import router from '@/services/router'
+import App from "@/App.vue";
+import router from "@/services/router";
 
-const app = createApp(App)
+const app = createApp(App);
 app.use(router);
-app.mount('#app')
+app.mount("#app");
