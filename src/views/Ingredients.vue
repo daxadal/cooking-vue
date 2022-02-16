e9384c1d437c52d19b34ba3efc48bf6329ebed8a
eric <eric@hamelyn.com>
2022-02-16 13:15:00 +0100

    Revert "Fix error handling"
    This reverts commit 8e873d2c4134c3310b665ed62350c8287927b1c9.

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -22,7 +22,7 @@ export default defineComponent({
     function showErrorModal(error: any) {
       isError.value = true;
       modalTitle.value = "Error";
-      modalMessage.value = error;
+      modalMessage.value = error?.message || error || "Ha ocurrido un error";
       isInfoVisible.value = true;
     }
 
