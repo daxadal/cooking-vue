fd10924547c2dfbefceb7034c68aa7825f0e0e51
eric <eric@hamelyn.com>
2022-02-16 13:44:37 +0100

    Use Vue SVG component

--- src/components/StepCarrousel.vue
+++ src/components/StepCarrousel.vue
@@ -4,11 +4,13 @@ import { defineComponent, PropType } from "vue";
 import { DetailedStep } from "@/resources/constants-types";
 import IngredientCard from "@/components/IngredientCard.vue";
 import UtensilCard from "@/components/UtensilCard.vue";
+import IconRightArrow from "@/icons/IconRightArrow.vue";
 
 export default defineComponent({
   components: {
     IngredientCard,
     UtensilCard,
+    IconRightArrow,
   },
   props: {
     step: {
@@ -22,9 +24,9 @@ export default defineComponent({
 <template>
   <div class="cards">
     <IngredientCard class="cards__card" :ingredient="step.input" />
-    <img class="cards__arrow" src="@/assets/right-arrow.svg" />
+    <IconRightArrow class="cards__arrow" />
     <UtensilCard class="cards__card" :utensil="step.utensil" />
-    <img class="cards__arrow" src="@/assets/right-arrow.svg" />
+    <IconRightArrow class="cards__arrow" />
     <IngredientCard class="cards__card" :ingredient="step.output" />
   </div>
 </template>
@@ -40,11 +42,8 @@ export default defineComponent({
   }
 
   &__arrow {
-    flex: 0 1 100px;
-    max-width: 100px;
-    max-height: 100px;
-    object-fit: contain;
-    color: var(--neutral-detail);
+    flex: 0 1 auto;
+    fill: var(--neutral-detail);
   }
 }
 </style>
